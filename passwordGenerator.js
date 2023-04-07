2a8fcadd4f20b0864e26381293bc765c40bf2877
Jack David Mullins <jackmullins06@gmail.com>
2023-04-07 18:32:41 +0100

    Added prompt so user can select password length.

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,43 +1,38 @@
-function generatePassword() {
-    var crypto = require("crypto");
-    var password = "";
-    var characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&()*+,-./:;<=>?@[]^_`{|}~";
+const crypto = require("crypto");
+const prompt = require('prompt-sync')();
 
-    for (let i = 0; i < 20; i++) {
-        let charIndex = Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1) * characters.length);
-        password += characters.charAt(charIndex);
-    }
+function promptForInput() {
+  const passwordLength = prompt("How long do you want your password to be? (Minumimum 16): ");
+  return passwordLength;
+}
 
-    return password;
+function generatePassword(passwordLength) {
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$%&()*+,-./:;<=>?@[]^_`{|}~";
+  let password = "";
+
+  for (let i = 0; i < passwordLength; i++) {
+    const charIndex = Math.floor(crypto.randomBytes(1)[0] / 256 * characters.length);
+    password += characters.charAt(charIndex);
+  }
+
+  return password;
 }
 
 function validatePassword(password) {
-    var has_uppercase = false;
-    var has_lowercase = false;
-    var has_number = false;
-    var has_special = false;
+  const has_uppercase = /[A-Z]/.test(password);
+  const has_lowercase = /[a-z]/.test(password);
+  const has_number = /\d/.test(password);
+  const has_special = /[!$%&()*+,-./:;<=>?@[\]^_`{|}~]/.test(password);
+  const has_comma = /,/.test(password);
 
-    for (let i = 0; i < password.length; i++) {
-        var char = password.charAt(i);
-        if (/[A-Z]/.test(char)) {
-            has_uppercase = true;
-        } else if (/[a-z]/.test(char)) {
-            has_lowercase = true;
-        } else if (/\d/.test(char)) {
-            has_number = true;
-        } else if (/[!@#$%^&*(),.?":{}|<>]/.test(char)) {
-            has_special = true;
-        }
-    }
-
-    return (password.length >= 20 && has_uppercase && has_lowercase && has_number && has_special);
+  return (password.length >= 16 && has_uppercase && has_lowercase && has_number && has_special && has_comma);
 }
 
-
-var password = generatePassword();
+const passwordLength = promptForInput(); // set the initial length to 16, you can change it to any desired length
+let password = generatePassword(passwordLength);
 
 while (!validatePassword(password)) {
-    password = generatePassword();
+  password = generatePassword(passwordLength);
 }
 
 console.log(password);
